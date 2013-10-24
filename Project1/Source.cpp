e844a74ce6c90af11e5f1a20f80c863e9377cd0c
Matt Larsen <larsen.matt1@gmail.com>
2013-10-24 08:43:13 -0700

    Added initial files

---
+++ Project1/Source.cpp
@@ -0,0 +1,436 @@
+// This example shows how to manually create a structured grid.
+// The basic idea is to instantiate vtkStructuredGrid, set its dimensions,
+// and then assign points defining the grid coordinate. The number of
+// points must equal the number of points implicit in the dimensions
+// (i.e., dimX*dimY*dimZ). Also, data attributes (either point or cell)
+// can be added to the dataset.
+//
+// 
+#include "vtkActor.h"
+#include "vtkCamera.h"
+#include "vtkFloatArray.h"
+#include "vtkHedgeHog.h"
+#include "vtkMath.h"
+#include "vtkPointData.h"
+#include "vtkPoints.h"
+#include "vtkPolyDataMapper.h"
+#include "vtkProperty.h"
+#include "vtkRenderWindow.h"
+#include "vtkRenderWindowInteractor.h"
+#include "vtkRenderer.h"
+#include "vtkStructuredGrid.h"
+#include <vtkImageData.h>
+#include <vtkPNGWriter.h>
+#include <vtkPointData.h>
+#include <vtkCellArray.h>
+#include <vtkPoints.h>
+#include <vtkUnsignedCharArray.h>
+#include <vtkFloatArray.h>
+#include <vtkDataSetReader.h>
+#include <vtkRectilinearGrid.h>
+#include <vtkFloatArray.h>
+#include <vtkPolyData.h>
+#include <vtkDataSetWriter.h>
+#include <vtkTubeFilter.h>
+#include <vtkPolyDataNormals.h>
+#include <vtkSphereSource.h>
+
+#include <vtkCamera.h>
+#include <vtkDataSetMapper.h>
+#include <vtkRenderer.h>
+#include <vtkActor.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkSmartPointer.h>
+
+
+
+
+
+// ****************************************************************************
+//  Function: GetNumberOfPoints
+//
+//  Arguments:
+//     dims: an array of size 3 with the number of points in X, Y, and Z.
+//           2D data sets would have Z=1
+//
+//  Returns:  the number of points in a rectilinear mesh
+//
+// ****************************************************************************
+
+int GetNumberOfPoints(const int *dims)
+{
+    // 3D
+    //return dims[0]*dims[1]*dims[2];
+    // 2D
+    return dims[0]*dims[1];
+}
+
+// ****************************************************************************
+//  Function: GetNumberOfCells
+//
+//  Arguments:
+//
+//      dims: an array of size 3 with the number of points in X, Y, and Z.
+//            2D data sets would have Z=1
+//
+//  Returns:  the number of cells in a rectilinear mesh
+//
+// ****************************************************************************
+
+int GetNumberOfCells(const int *dims)
+{
+    // 3D
+    //return (dims[0]-1)*(dims[1]-1)*(dims[2]-1);
+    // 2D
+    return (dims[0]-1)*(dims[1]-1);
+}
+
+
+// ****************************************************************************
+//  Function: GetPointIndex
+//
+//  Arguments:
+//      idx:  the logical index of a point.
+//              0 <= idx[0] < dims[0]
+//              1 <= idx[1] < dims[1]
+//              2 <= idx[2] < dims[2] (or always 0 if 2D)
+//      dims: an array of size 3 with the number of points in X, Y, and Z.
+//            2D data sets would have Z=1
+//
+//  Returns:  the point index
+//
+// ****************************************************************************
+
+int GetPointIndex(const int *idx, const int *dims)
+{
+    // 3D
+    //return idx[2]*dims[0]*dims[1]+idx[1]*dims[0]+idx[0];
+    // 2D
+    return idx[1]*dims[0]+idx[0];
+}
+
+
+// ****************************************************************************
+//  Function: GetCellIndex
+//
+//  Arguments:
+//      idx:  the logical index of a cell.
+//              0 <= idx[0] < dims[0]-1
+//              1 <= idx[1] < dims[1]-1 
+//              2 <= idx[2] < dims[2]-1 (or always 0 if 2D)
+//      dims: an array of size 3 with the number of points in X, Y, and Z.
+//            2D data sets would have Z=1
+//
+//  Returns:  the cell index
+//
+// ****************************************************************************
+
+int GetCellIndex(const int *idx, const int *dims)
+{
+    // 3D
+    //return idx[2]*(dims[0]-1)*(dims[1]-1)+idx[1]*(dims[0]-1)+idx[0];
+    // 2D
+    return idx[1]*(dims[0]-1)+idx[0];
+}
+
+// ****************************************************************************
+//  Function: GetLogicalPointIndex
+//
+//  Arguments:
+//      idx (output):  the logical index of the point.
+//              0 <= idx[0] < dims[0]
+//              1 <= idx[1] < dims[1] 
+//              2 <= idx[2] < dims[2] (or always 0 if 2D)
+//      pointId:  a number between 0 and (GetNumberOfPoints(dims)-1).
+//      dims: an array of size 3 with the number of points in X, Y, and Z.
+//            2D data sets would have Z=1
+//
+//  Returns:  None (argument idx is output)
+//
+// ****************************************************************************
+
+void GetLogicalPointIndex(int *idx, int pointId, const int *dims)
+{
+    // 3D
+    // idx[0] = pointId%dim[0];
+    // idx[1] = (pointId/dims[0])%dims[1];
+    // idx[2] = pointId/(dims[0]*dims[1]);
+
+    // 2D
+    idx[0] = pointId%dims[0];
+    idx[1] = pointId/dims[0];
+}
+
+
+// ****************************************************************************
+//  Function: GetLogicalCellIndex
+//
+//  Arguments:
+//      idx (output):  the logical index of the cell index.
+//              0 <= idx[0] < dims[0]-1
+//              1 <= idx[1] < dims[1]-1 
+//              2 <= idx[2] < dims[2]-1 (or always 0 if 2D)
+//      cellId:  a number between 0 and (GetNumberOfCells(dims)-1).
+//      dims: an array of size 3 with the number of points in X, Y, and Z.
+//            2D data sets would have Z=1
+//
+//  Returns:  None (argument idx is output)
+//
+// ****************************************************************************
+
+void GetLogicalCellIndex(int *idx, int cellId, const int *dims)
+{
+    // 3D
+    // idx[0] = cellId%(dims[0]-1);
+    // idx[1] = (cellId/(dims[0]-1))%(dims[1]-1);
+    // idx[2] = cellId/((dims[0]-1)*(dims[1]-1));
+
+    // 2D
+    idx[0] = cellId%(dims[0]-1);
+    idx[1] = cellId/(dims[0]-1);
+}
+
+
+// ****************************************************************************
+//  Function: EvaluateVectorFieldAtLocation
+//
+//  Arguments:
+//     pt: a two-dimensional location
+//     dims: an array of size two.  
+//              The first number is the size of the array in argument X, 
+//              the second the size of Y.
+//     X: an array (size is specified by dims).  
+//              This contains the X locations of a rectilinear mesh.
+//     Y: an array (size is specified by dims).  
+//              This contains the Y locations of a rectilinear mesh.
+//     F: a vector field defined on the mesh.  Its size is 2*dims[0]*dims[1].
+//        The first value in the field is the x-component for the first point.
+//        The second value in the field is the y-component for the first point.
+//
+//     rv (output): the interpolated field value. (0,0) if the location is out of bounds.
+//
+// ****************************************************************************
+
+void EvaluateVectorFieldAtLocation(const float *pt, const int *dims, const float *X, 
+                              const float *Y, const float *F, float *rv)
+{
+    // IMPLEMENT ME!
+
+    rv[0] = 0; // setting the x-component of the velocity
+    rv[1] = 0; // setting the y-component of the velocity
+}
+
+// ****************************************************************************
+//  Function: AdvectWithEulerStep
+//
+//  Arguments:
+//     pt: the seed location (two-dimensions)
+//     dims: an array of size two.  
+//              The first number is the size of the array in argument X, 
+//              the second the size of Y.
+//     X: an array (size is specified by dims).  
+//              This contains the X locations of a rectilinear mesh.
+//     Y: an array (size is specified by dims).  
+//              This contains the Y locations of a rectilinear mesh.
+//     F: a vector field defined on the mesh.  Its size is 2*dims[0]*dims[1].
+//     h: The size of the Euler step
+//     nsteps: The number of Euler steps to take
+//     output_locations (output): An array of size 2*(nsteps+1).  It's first entry
+//        should be the seed location.  The second entry should be the result
+//        of the first advection step.  The final entry should be the result
+//        of the final advection step.
+//     speeds (output): An array of size (nsteps+1).  It's first entry should be the
+//        speed at the seed location.  It's final entry should be the speed
+//        at the location of the result of the final advection step.
+//        Recall that speed is the magnitude of the velocity.
+//
+// ****************************************************************************
+
+void
+AdvectWithEulerStep(const float *pt, const int *dims, const float *X, 
+                    const float *Y, const float *F, 
+                    float h, int nsteps, float *output_locations, float *speeds)
+{
+    // IMPLEMENT ME!
+
+    output_locations[0] = 0; // set the x component of the first output location
+    output_locations[1] = 0; // set the y component of the first output location
+    output_locations[2] = 0; // set the x component of the second output location
+    output_locations[3] = 0; // set the y component of the second output location
+    // ...
+    speeds[0] = 0; // set the speed at the first output location
+    speeds[1] = 0; // set the speed at the first output location
+    // ...
+}
+
+// ****************************************************************************
+//  Function: CalculateArcLength
+//
+//  Arguments:
+//     locations: an array of 2D locations.
+//     nlocations: the number of locations in the array "locations".
+//
+//  Returns: the arc length, meaning the distance between each successive
+//           pair of points
+//
+// ****************************************************************************
+
+float
+CalculateArcLength(const float *output_locations, int nlocations)
+{
+    // IMPLEMENT ME!
+    return 0;
+}
+
+void
+WriteImage(vtkImageData *img, const char *filename)
+{
+    std::string full_filename = filename;
+    full_filename += ".png";
+    vtkPNGWriter *writer = vtkPNGWriter::New();
+    writer->SetInput(img);
+    writer->SetFileName(full_filename.c_str());
+    writer->Write();
+    writer->Delete();
+}
+
+vtkImageData *
+NewImage(int width, int height)
+{
+    vtkImageData *image = vtkImageData::New();
+    image->SetDimensions(width, height, 1);
+    image->SetWholeExtent(0, width-1, 0, height-1, 0, 0);
+    image->SetUpdateExtent(0, width-1, 0, height-1, 0, 0);
+    image->SetNumberOfScalarComponents(3);
+    image->SetScalarType(VTK_UNSIGNED_CHAR);
+    image->AllocateScalars();
+
+    return image;
+}
+
+// VTK files are only 3D, so the vector data is all of the form (X,Y,0).
+// Remove the 0's since it is counter-intuitive for students who are 
+// thinking of this as 2D data.
+float *
+Convert3DVectorDataTo2DVectorData(const int *dims, const float *F)
+{
+    float *rv = new float[dims[0]*dims[1]*2];
+    int index3D = 0;
+    int index2D = 0;
+    for (int i = 0 ; i < dims[0] ; i++)
+       for (int j = 0 ; j < dims[1] ; j++)
+       {
+           rv[index2D]   = F[index3D];
+           rv[index2D+1] = F[index3D+1];
+           index2D += 2;
+           index3D += 3;
+       }
+
+    return rv;
+}
+
+vtkPolyData *
+CreateVTKPolyData(int nseeds, int nsteps, float **output_locations, float **speeds)
+{
+    int numPoints = nseeds*(nsteps+1);
+    vtkPoints *pts = vtkPoints::New();
+    pts->SetNumberOfPoints(numPoints);
+    vtkFloatArray *var = vtkFloatArray::New();
+    var->SetName("speed");
+    var->SetNumberOfTuples(numPoints);
+    int ptIdx = 0;
+    vtkCellArray *lines = vtkCellArray::New();
+    lines->EstimateSize(numPoints,2);
+    for (int i = 0 ; i < nseeds ; i++)
+    {
+        for (int j = 0 ; j < nsteps+1 ; j++)
+        {
+            double pt[3];
+            pt[0] = output_locations[i][2*j];
+            pt[1] = output_locations[i][2*j+1];
+            pt[2] = 0.;
+            pts->SetPoint(ptIdx, pt);
+            var->SetTuple1(ptIdx, speeds[i][j]);
+            if (j > 0)
+            {
+                vtkIdType ids[2] = { ptIdx-1, ptIdx };
+                lines->InsertNextCell(2, ids);
+            }
+            ptIdx++;
+        }
+    }
+
+    vtkPolyData *pd = vtkPolyData::New();
+    pd->SetPoints(pts);
+    pd->GetPointData()->AddArray(var);
+    pd->GetPointData()->SetActiveScalars("speed");
+    pd->SetLines(lines);
+    lines->Delete();
+    var->Delete();
+    pts->Delete();
+
+    return pd;
+}
+ 
+int main(int, char*[])
+{
+  
+	vtkDataSetReader *rdr = vtkDataSetReader::New();
+    rdr->SetFileName("output.vtk");
+    rdr->Update();
+	
+	int dims[3];
+	vtkRectilinearGrid *rgrid = (vtkRectilinearGrid *) rdr->GetOutput();
+	rgrid->GetDimensions(dims);
+
+
+  
+
+  // We create a simple pipeline to display the data.
+  vtkHedgeHog *hedgehog = vtkHedgeHog::New();
+      hedgehog->SetInput(rgrid);
+      hedgehog->SetScaleFactor(0.1);
+	  hedgehog->SetScaleFactor(2.0);
+
+  vtkPolyDataMapper *sgridMapper = vtkPolyDataMapper::New();
+      sgridMapper->SetInputConnection(hedgehog->GetOutputPort());
+  vtkActor *sgridActor = vtkActor::New();
+      sgridActor->SetMapper(sgridMapper);
+      sgridActor->GetProperty()->SetColor(0,0,0);
+
+  // Create the usual rendering stuff
+  vtkRenderer *renderer = vtkRenderer::New();
+  vtkRenderWindow *renWin = vtkRenderWindow::New();
+    renWin->AddRenderer(renderer);
+
+  vtkRenderWindowInteractor *iren = vtkRenderWindowInteractor::New();
+    iren->SetRenderWindow(renWin);
+
+  renderer->AddActor(sgridActor);
+  renderer->SetBackground(1,1,1);
+  renderer->ResetCamera();
+  //renderer->GetActiveCamera()->SetClippingRange(20, 120);
+  renderer->GetActiveCamera()->SetViewUp(0,1,0);
+  renderer->GetActiveCamera()->SetClippingRange(20, 120);
+  //renderer->GetActiveCamera()->Azimuth(30.0);
+  renderer->GetActiveCamera()->Zoom(2.25);
+  renderer->GetActiveCamera()->SetDistance(30);
+  renWin->SetSize(800,800);
+
+  // interact with data
+  renWin->Render();
+  iren->Start();
+
+  renderer->Delete();
+  renWin->Delete();
+  iren->Delete();
+  //sgrid->Delete();
+  hedgehog->Delete();
+  sgridMapper->Delete();
+  sgridActor->Delete();
+
+ 
+  return EXIT_SUCCESS;
+}
+ 
